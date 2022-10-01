a97c9a4c794b702aa71d1eb46539095f9f598af3
Dipayan <ghosedipayen@gmail.com>
2022-10-01 12:40:33 +0600

    adding right side

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -23,7 +23,26 @@ const Meals = () => {
             }
           </div>
           <div className='function'>
-            <h2>Functions</h2>
+            <div className='myInfo'>
+                 <div >
+                   <img className='myPic' src="https://media.istockphoto.com/photos/young-man-portrait-picture-id483265467?k=20&m=483265467&s=612x612&w=0&h=C38xiYdEiJ5OTvmgrh7zrfVPEFdllv0I5FF9Picjzic="></img>
+                 </div>
+                <div>
+                 <h5 className='dg'>Dipayan Ghose</h5>
+                 <p>Chittagong, Bangladesh</p>
+                </div>
+            </div>
+            <div className='myBio'>
+              <div>
+                <h4>84 kg</h4>Weight
+              </div>
+              <div>
+              <h4>5.10"</h4>Height
+              </div>
+              <div>
+              <h4>25 yrs</h4>Age
+              </div>
+            </div>
           </div>
         </div>
         </div>
